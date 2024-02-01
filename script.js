fa66a57debf6280b4caeb076e8a3257d3501f80b
音猫｡ <116952822+otoneko1102@users.noreply.github.com>
2024-02-01 20:34:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  generate("音猫｡", "otoneko.", "Everything is cats,\nCats are everything.", "https://otoneko1102.github.io/miq-generator/img/otoneko.webp");
-});
-
-function generate(firstDisplayName, firstName, firstContent, firstIcon) {
-  const displayName = (firstDisplayName !== undefined && firstDisplayName !== null) ? firstDisplayName : document.getElementById("displayName").value || null;
-  const name = (firstName !== undefined && firstName !== null) ? firstName : document.getElementById("name").value || null;
-  const content = (firstContent !== undefined && firstContent !== null) ? firstContent : document.getElementById("content").value || null;
+function generate() {
+  const displayName = document.getElementById("displayName").value || null;
+  const name = document.getElementById("name").value || null;
+  const content = document.getElementById("content").value || null;
   const option = document.getElementById("option").value;
-  const icon = (firstIcon !== undefined && firstIcon !== null) ? firstIcon : document.getElementById("iconUrl").value || null;
+  const icon = document.getElementById("iconUrl").value || null;
 
   fetch("https://api.voids.top/fakequote", {
     method: "POST",
@@ -37,5 +33,6 @@ function displayImage(imageUrl) {
   const generatedImageContainer = document.getElementById("generatedImage");
   const imgElement = document.createElement("img");
   imgElement.src = imageUrl;
+  generatedImageContainer.innerHTML = '';
   generatedImageContainer.appendChild(imgElement);
 }
